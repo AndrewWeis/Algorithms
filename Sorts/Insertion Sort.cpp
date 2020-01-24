afdaf237f91e1a4bb31a994292f1c48e5c48fbec
AndrewWeis <54636107+AndrewWeis@users.noreply.github.com>
2020-01-24 21:57:19 +0700

    Update Insertion Sort.cpp

--- Sorts/Insertion Sort.cpp
+++ Sorts/Insertion Sort.cpp
@@ -1,6 +1,6 @@
-// Insertion Sort 
+Insertion Sort 
 
-/*Time Complexity: O(n*2)   Best case: O(n) Average case: O(n^2) Worst case: O(n^2)
+Time Complexity: O(n*2)   Best case: O(n) Average case: O(n^2) Worst case: O(n^2)
 
 Boundary Cases: Insertion sort takes maximum time to sort if elements are sorted in reverse order. 
 And it takes minimum time (Order of n) when elements are already sorted.
