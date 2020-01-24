34cc822a8509cae55337dbb2f3cb3fb263b19e1b
AndrewWeis <54636107+AndrewWeis@users.noreply.github.com>
2020-01-24 21:57:30 +0700

    Update Insertion Sort.cpp

--- Sorts/Insertion Sort.cpp
+++ Sorts/Insertion Sort.cpp
@@ -6,7 +6,7 @@ Boundary Cases: Insertion sort takes maximum time to sort if elements are sorted
 And it takes minimum time (Order of n) when elements are already sorted.
 
 Uses: Insertion sort is used when number of elements is small. 
-It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array. */
+It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array. 
 
 
 #include <iostream>
