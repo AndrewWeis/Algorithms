75a640c266110ea65c5a871aed6b3e8c1ce2cc7a
AndrewWeis <54636107+AndrewWeis@users.noreply.github.com>
2020-01-26 16:25:08 +0700

    Update MergeSort.cpp

--- Sorts/MergeSort.cpp
+++ Sorts/MergeSort.cpp
@@ -1,69 +1,91 @@
-#include <iostream>
-#include <stdlib.h>
-#include <string.h>
+
+
+
+#include <iostream> 
 
 using namespace std;
 
-void merge(int* a, int ak, int* b, int bk, int* res) {
-	int aid = 0, bid = 0;
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
-	while (aid < ak && bid < bk) {
-		
-		if (a[aid] < b[bid]) {
-			res[aid + bid] = a[aid];
-			aid++;
-		}
-		else {
-			res[aid + bid] = b[bid];
-			bid++;
-		}
-	}
+    int* L = new int[n1];
+    int* R = new int[n2];
 
-	while (aid < ak)
-		res[aid + bid] = a[aid++];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
 
-	while (bid < bk)
-		res[aid + bid] = b[bid++];
+    i = 0; 
+    j = 0; 
+    k = l; 
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+    delete[] R;
+    delete[] L; 
 }
 
-void mergeSort(int ary[], int size) {
-	
-	int* tmp = (int*)malloc(size * sizeof(int));
-	
-	if (size <= 1)
-		return;
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+     
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
 
-	const int mid = size / 2;
+        merge(arr, l, m, r);
+    }
+}
 
-	mergeSort(ary, mid);
-	mergeSort(ary + mid, size - mid);
-	merge(ary, mid, ary + mid, size - mid, tmp);
-
-	memcpy(ary, tmp, size * sizeof(int));
+void printArray(int A[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << A[i] << " ";
+    cout << "\n";
 }
 
 
-int main() {
-	int N;
+int main()
+{
+    int arr[] = { 38, 27, 43, 3, 9, 82, 10 };
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
 
-	cout << "Enter number of elements in array: ";
-	cin >> N;
-	
-	int* arr = (int*)malloc(N * sizeof(int));
+    printf("Given array is ");
+    printArray(arr, arr_size);
 
-	cout << "Enter " << N << " elements in array: ";
+    mergeSort(arr, 0, arr_size - 1);
 
-	for (int i = 0; i < N; i++)
-		cin >> arr[i];
-
-	mergeSort(arr, N);
-
-	cout << "Your sorted array: ";
-
-	for (int i = 0; i < N; i++)
-		cout << arr[i] << " ";
-
-	free(arr);
-
-	return 0;
+    printf("\nSorted array is ");
+    printArray(arr, arr_size);
+    return 0;
 }
