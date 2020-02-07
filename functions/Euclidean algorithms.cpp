f918b5425a4d682395a2d015ca3a2fc050eafece
AndrewWeis <54636107+AndrewWeis@users.noreply.github.com>
2020-02-07 13:04:47 +0700

    Update Euclidean algorithms.cpp

--- functions/Euclidean algorithms.cpp
+++ functions/Euclidean algorithms.cpp
@@ -4,3 +4,27 @@ int gcd(int a, int b)
 {
 	return (b == 0 ? a : gcd(b, a % b));
 }
+
+// Extended Eucliadean algorithm
+
+int gcdExtended(int a, int b, int *x, int *y)  
+{  
+    // Base Case  
+    if (a == 0)  
+    {  
+        *x = 0;  
+        *y = 1;  
+        return b;  
+    }  
+  
+    int x1, y1; // To store results of recursive call  
+    int gcd = gcdExtended(b%a, a, &x1, &y1);  
+  
+    // Update x and y using results of  
+    // recursive call  
+    *x = y1 - (b/a) * x1;  
+    *y = x1;  
+  
+    return gcd;  
+}  
+
